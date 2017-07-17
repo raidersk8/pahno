7495590d07df3d474f6bfa9df3404c35d978a426
raidersk8 <romangalchenkov@gmail.com>
2017-07-17 08:29:00 +0300

    update

--- blocks/base/product-item.php
+++ blocks/base/product-item.php
@@ -1,5 +1,5 @@
 <a href="<?php echo get_the_permalink ( get_the_ID() ); ?>" class="product-item">
-		<div class="img to-the-right">
+		<div class="img">
 			<?php if(has_post_thumbnail(get_the_ID())) : ?>
 				<img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'image-905-auto' ); ?>" />
 			<?php endif; ?>
